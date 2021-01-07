ef044d974137a1496f4f0e3e47727382ff512aeb
drmrhdt <drmrhdt@gmail.com>
2021-01-07 14:44:48 +0300

    refactor: replace Uni with Mfc everywhere

--- MFC/Mfc/asgi.py
+++ MFC/Mfc/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Uni project.
+ASGI config for Mfc project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Uni.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mfc.settings')
 
 application = get_asgi_application()
--- MFC/Mfc/wsgi.py
+++ MFC/Mfc/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Uni project.
+WSGI config for Mfc project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Uni.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mfc.settings')
 
 application = get_wsgi_application()
